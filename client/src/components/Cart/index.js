550290d99a0ae2f43ed0b285806f7ec40cfc8fb3
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-10-21 11:00:38 -0400

    [ISSUE #3] -complete

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      // dispatch the ADD_MULTIPLE_TO_CART action here because we have an array of items returning from IndexedDB, even if it's just one product saved.
+      // This way we can just dump all of the products into the global state object at once instead of doing it one by one
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    }
+
+    if (!state.cart.length) {
+      getCart();
+    }
+    /**
+     * pass the state.cart.length value into useEffect()'s dependency array.
+     * this prevents the useEffect() from contiuously looping
+     * Through the dependency array, list all of the data that this useEffect() Hook is dependent on to execute
+     * The Hook runs on load no matter what, but then it only runs again if any value in the dependency array has changed since the last time it ran.
+     */
+  }, [state.cart.length, dispatch]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
